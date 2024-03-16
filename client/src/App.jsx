1e291f8959cca23137b1ca0091e3f4529f13f729
adrian.taay <adrian.tm@live.com>
2024-03-16 16:17:11 +0800

    reverted to an earlier commit. started calculator app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,14 @@ import { useState } from "react";
 import Taskbar from "./components/Taskbar";
 import Window from "./components/Windows/Window";
 import WelcomeScreen from "./components/Windows/WelcomeScreen";
-import Test from "./components/Windows/Test";
-
-import OpenWindowProvider from "./OpenWindowsProvider";
+import Calculator from "./components/Calculator/Calculator";
 
 function App() {
   const [showWindow, setShowWindow] = useState(false);
 
   return (
     <>
-      <OpenWindowProvider>
-        <Window />
-        <Taskbar />
-      </OpenWindowProvider>
-      <div className="w-full h-screen bg-[url('./assets/bg-wallpaper.jpg')] bg-cover bg-center overflow-hidden">
+      <div className="w-full h-screen bg-[url('./assets/bg-wallpaper.jpg')] bg-cover bg-center">
         <button onClick={() => setShowWindow(!showWindow)}>Show</button>
         {showWindow ? (
           <>
@@ -27,6 +21,10 @@ function App() {
           </>
         ) : null}
       </div>
+      <div className="calculator absolute top-1/3 left-1/4">
+        <Calculator />
+      </div>
+      <Taskbar />
     </>
   );
 }
