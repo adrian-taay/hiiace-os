dad434cb0af70faba3c9d8c150d615f001f2ea7d
adrian.taay <adrian.tm@live.com>
2024-03-21 19:27:29 +0800

    update: started useReducer

--- client/src/OpenAppsProvider.jsx
+++ client/src/OpenAppsProvider.jsx
@@ -1,9 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
+import { menu } from "./components/StartMenu/menu";
 
 export const OpenAppsContext = createContext();
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'open-app':
+      return {
+        ...state,
+        {
+          
+        }
+      }
+  }
+}
+
 export default function OpenAppsProvider({ children }) {
-  const [openApps, setOpenApps] = useState([]);
+  // const [openApps, setOpenApps] = useState([]);
+  const [state, dispatch] = useReducer(reducer, []);
 
   function updateOpenApps(newApp) {
     setOpenApps([...openApps, newApp]);
--- client/src/components/StartMenu/menu.jsx
+++ client/src/components/StartMenu/menu.jsx
@@ -10,36 +10,43 @@ const iconSize = 24;
 
 export const menu = [
   {
+    id: 0,
     title: "Documents",
     content: <Test />,
     icon: <LuAppWindow size={iconSize} />,
   },
   {
+    id: 1,
     title: "Welcome Screen",
     content: <WelcomeScreen />,
     icon: <LuPartyPopper size={iconSize} />,
   },
   {
+    id: 2,
     title: "Calculator",
     content: <Calculator />,
     icon: <CgCalculator size={iconSize} />,
   },
   {
+    id: 3,
     title: "Spotiphy",
     content: "",
     icon: <FaSpotify size={iconSize} />,
   },
   {
+    id: 4,
     title: "Mathel Memory",
     content: "",
     icon: <LuBrain size={iconSize} />,
   },
   {
+    id: 5,
     title: "Terminal",
     content: "",
     icon: <PiTerminalWindow size={iconSize} />,
   },
   {
+    id: 6,
     title: "About",
     content: "",
     icon: <LuInfo size={iconSize} />,
